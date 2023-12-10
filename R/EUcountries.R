f0d4d8d926991325c26840a56d6e9b91a6bb2aa7
Baptiste Yzern <yzern.baptiste@outlook.com>
2023-12-10 13:50:16 +0100

    added Countries data

---
+++ R/EUcountries.R
@@ -0,0 +1,17 @@
+#' EU countries and their labels
+#'
+#'
+#' @format ## `EUcountries`
+#' A data frame with 32 rows and 7 columns:
+#' \describe{
+#'   \item{abreviation}{2 letter ISO country codes}
+#'   \item{country}{Nom du pays en anglais ou en langue origianle}
+#'   \item{FRcountry}{Nom du pays en français}
+#'   \item{year2010}{Participation du pays à la vague 2010 de l'EUSILC}
+#'   \item{year2011}{Participation du pays à la vague 2011 de l'EUSILC}
+#'   \item{year2012}{Participation du pays à la vague 2012 de l'EUSILC}
+#'   \item{year2013}{Participation du pays à la vague 2013 de l'EUSILC}
+#'   ...
+#' }
+#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
+"EUcountries"
