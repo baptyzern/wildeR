4eda9c346fe12eb0d52ec0d069240b9ec23c660d
bouh40 <142528128+bouh40@users.noreply.github.com>
2024-01-24 16:21:46 +0100

    add pointmedian() and guillemetsFR()

---
+++ R/guillemetsFR.R
@@ -0,0 +1,7 @@
+#' Guillemets français
+#'
+#' @return
+#' @export
+#'
+#' @examples
+guillemetsFR <- function() {message("Here comes the guillemets : · « ... » ·")}
---
+++ R/pointmedian.R
@@ -0,0 +1,8 @@
+#' Point médian
+#'
+#' @return
+#' @export
+#'
+#' @examples
+pointmedian <- function() {message("Here comes the point médian : « · » ")}
+
